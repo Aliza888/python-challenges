80bb1843e08ccbc1b58fdc995f5500e99cb77781
Aliza888 <a.liza00336@gmail.com>
2025-02-25 15:27:44 +0500

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,7 @@
+print("Hello, Python Learners! 🚀 Today is the first step in our coding journey."
+ )
+
+
+# Bonus Challenge
+name = input("Enter your name: ")  
+print(f"Hello, {name}! 🚀 Today is the first step in our coding journey.")
